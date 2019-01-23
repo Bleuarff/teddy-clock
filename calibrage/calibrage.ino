64c237c76262cf87478739fe4acb9278a461bd76
bleuarff <bleuarff@gmail.com>
2019-01-23 23:33:06 +0100

    Fix calibration sketch

--- calibrage/calibrage.ino
+++ calibrage/calibrage.ino
@@ -1,3 +1,14 @@
+/**************************************************************************
+
+  Creates public SSID Hérisson & a web server at 192.168.14.1:80
+
+  Endpoints:
+  - GET /sense : current light level
+  - POST /led: set led. Payload is r=X&g=Y&b=Z
+    X, Y, Z being the values for red, green and blue channels
+***************************************************************************/
+
+
 #include <ESP8266WebServer.h>
 #include <ESP8266WiFi.h>
 
@@ -7,6 +18,28 @@ const byte redPin = 14;
 const byte greenPin = 13;
 const byte bluePin = 12;
 
+// Read current light level and returns it
+void getSense(){
+  Serial.print("GET /sense: ");
+  int lvl = analogRead(A0);
+  Serial.printf("%i\n", lvl);
+  String s = String(lvl);
+  s += "\n";
+  server.send(200, "text/plain", s);
+}
+
+// Sets led to given rgb value
+void setLed(){
+  Serial.print("POST /led: ");
+  int red = server.arg("r").toInt();
+  int green = server.arg("g").toInt();
+  int blue = server.arg("b").toInt();
+  Serial.printf("r:%i  g:%i  b:%i\n", red, green, blue);
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue);
+  server.send(204, "text/plain", "");
+}
 
 void setup(){
   Serial.begin(115200);
@@ -35,26 +68,8 @@ void setup(){
   Serial.println("HTTP server started");
 }
 
-// Read current light level and returns it
-void getSense(){
-  int lvl = analogRead(A0);
-  String s = String(lvl);
-  server.send(200, "text/plain", s);
-}
 
-// Sets led to given rgb value
-void setLed(){
-  int red = server.arg("r").toInt();
-  int green = server.arg("g").toInt();
-  int blue = server.arg("b").toInt();
-
-  Serial.printf("r:%s   g:%s  b:%s", red, green, blue);
-  analogWrite(redPin, red);
-  analogWrite(greenPin, green);
-  analogWrite(bluePin, blue);
-  server.send(204, "text/plain", "");
-}
 
 void loop(){
-
+  server.handleClient();
 }
